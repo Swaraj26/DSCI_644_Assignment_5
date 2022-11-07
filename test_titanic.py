bb2a79fa24970eb6eea606c5751d15244cfec113
Swaraj Yadav <swarajy26@gmail.com>
2022-11-07 16:07:14 -0500

    First Failed Test

---
+++ test_titanic.py
@@ -0,0 +1,16 @@
+import pytest
+import os.path
+from os import path
+from titanic_a5 import file_input
+from titanic_a5 import fail_file_input
+
+def test_file_input():
+    data,test_data = file_input()
+    assert len(data)!=0
+    assert len(test_data)!=0
+    
+
+def test_fail_file_input():
+    data,test_data = fail_file_input()
+    assert data!=data.empty
+    assert test_data!=test_data.empty
--- titanic_a5.py
+++ titanic_a5.py
@@ -5,21 +5,41 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 
-data = pd.read_csv("C:/Users/swara/OneDrive/HW/SWDS/hw/Assignment_5/train.csv")
-test_data = pd.read_csv("C:/Users/swara/OneDrive/HW/SWDS/hw/Assignment_5/test.csv")
+def fail_file_input():
+    data = pd.read_csv("C:/Users/swara/OneDrive/HW/SWDS/hw/Assignment_5/training.csv")
+    test_data = pd.read_csv("C:/Users/swara/OneDrive/HW/SWDS/hw/Assignment_5/test.csv")
+    return data,test_data
 
-y = data['Survived']
-features = ['Pclass','Sex','SibSp','Parch']
-x = pd.get_dummies(data[features])
-# X_test = pd.get_dummies(test_data[features])
-# y_test = test_data[]
+def file_input():
+    data = pd.read_csv("C:/Users/swara/OneDrive/HW/SWDS/hw/Assignment_5/train.csv")
+    test_data = pd.read_csv("C:/Users/swara/OneDrive/HW/SWDS/hw/Assignment_5/test.csv")
+    return data,test_data
 
-xtrain,xtest,ytrain,ytest = train_test_split(x,y,test_size=0.25,random_state=420)
+def train_test_splitter(data):
+    y = data['Survived']
+    features = ['Pclass','Sex','SibSp','Parch']
+    x = pd.get_dummies(data[features])
+    # X_test = pd.get_dummies(test_data[features])
+    # y_test = test_data[]
 
-model = RandomForestClassifier(n_estimators=100,max_depth=4,random_state=69)
+    xtrain,xtest,ytrain,ytest = train_test_split(x,y,test_size=0.25,random_state=420)
+    return xtrain,xtest,ytrain,ytest
 
-model.fit(xtrain,ytrain)
-predictions = model.predict(xtest)
+def model_gen(xtrain,ytrain):
+    model = RandomForestClassifier(n_estimators=100,max_depth=4,random_state=69)
+    model.fit(xtrain,ytrain)
+    return model
 
-print("Accuracy = ",round((accuracy_score(ytest,predictions)*100),4),"%") 
-print(" ")
+def preds(model,xtest,ytest):
+    predictions = model.predict(xtest)
+    print("Accuracy = ",round((accuracy_score(ytest,predictions)*100),4),"%") 
+
+def main():
+    data,test_data = file_input()
+    xtrain,xtest,ytrain,ytest = train_test_splitter(data)
+    model = model_gen(xtrain,ytrain)
+    preds(model,xtest,ytest)
+
+if __name__ == "__main__":
+    main()
+
