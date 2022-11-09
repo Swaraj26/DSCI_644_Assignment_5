6c338daf602f5191ecadcc00964809930dc6a2c2
Swaraj Yadav <swarajy26@gmail.com>
2022-11-08 21:05:20 -0500

    Added all test functions

--- test_titanic.py
+++ test_titanic.py
@@ -14,3 +14,12 @@ def test_fail_file_input():
     data,test_data = fail_file_input()
     assert data!=data.empty
     assert test_data!=test_data.empty
+
+def test_train_test_splitter():
+    assert 0
+
+def test_model_gen():
+    assert 0
+
+def test_preds():
+    assert 0
